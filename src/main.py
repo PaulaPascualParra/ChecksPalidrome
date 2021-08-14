619921f09b1ba4146329816b2c8ec96c699e2caa
Paula Pascual <paulapascual@Mini-de-Paula.technicolor.net>
2021-08-15 00:34:37 +0200

    exe created

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import helper
 import function
 
 if __name__ == '__main__':
-    # todo: create an interface with pyqt
     input_data = helper.get_input_data()
     print(function.is_palindrome_reverse(input_data))
     print(function.is_palindrome_queue(input_data))
